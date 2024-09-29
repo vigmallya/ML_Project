89c67f400e7881724d94e2eac14165075dae00b6
Vignesh Mallya <vignesh.mallya315@gmail.com>
2024-09-29 06:25:28 +0200

    Model training Implementation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass #used to create class variables
 
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 #Different Input for DataIngestion(where we need to save test and train data)
@@ -56,3 +56,6 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,12 @@ import sys
 
 import numpy as np 
 import pandas as pd
-import pickle #helps to create pickle file
+import pickle #Helps to create pickle file
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
+#For creating pickle file
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -15,6 +17,32 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+#Evaluating models and returns list of models with its prediction rate.
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
 
     except Exception as e:
         raise CustomException(e, sys)
