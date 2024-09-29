6e217f47709451ad5f683f460849116a1f147d93
Vignesh Mallya <vignesh.mallya315@gmail.com>
2024-09-29 11:03:52 +0200

    Prediction pipeline with Flask Web

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 
 import numpy as np 
 import pandas as pd
-import pickle #Helps to create pickle file
+import pickle #Helps to create/read pickle file
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -55,6 +55,15 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
 
         return report
     
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+#Read data from pkl file
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
